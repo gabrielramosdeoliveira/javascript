928749192d4576634f6ad2148277d2fa1396f7bc
gabrielramosdeoliveira <designersoliveira8@gmail.com>
2025-03-11 18:58:06 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,17 @@
 var c = 1
-while (c < 6) {
-    console.log('tudo bem?')
+ do {
+    console.log(`passo ${c}`)
     c++
-}
+} while (c < 6)
 
 /*console.log('tudo bem?')
 console.log('tudo bem?')
 console.log('tudo bem?')
 console.log('tudo bem?')
 console.log('tudo bem?')*/
+
+/*var c = 1
+while (c < 6) {
+    console.log(`passo ${c}`)
+    c++
+}*/
