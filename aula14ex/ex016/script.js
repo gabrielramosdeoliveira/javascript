21fdf5e94ceb84ca2628a6c11c9421dbf3949db5
gabrielramosdeoliveira <designersoliveira8@gmail.com>
2025-03-15 12:44:43 -0300

    e

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,19 +4,25 @@ function contar() {
     let passo = document.getElementById('pas')
     let res = document.getElementById('res')
 
-    if (ini.value == 0 || fim.value == 0 || passo.value == 0 ) {
-        window.alert('[ERRO] faltam dados!')
+    if (ini.value == 0 || fim.value == 0) {
+        res.innerHTML = 'impossível contar'
+       // window.alert('[ERRO] faltam dados!')   
     } else {
-        res.innerHTML = 'Contando:  '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
+        if (p <= 0) {
+            window.alert('passo inválido Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
+            //contagem crescente
             for(let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449}`
             }
         } else {
+            //contagem regressiva
             for(let c = i; c >= f; c -= p) {
                 res.innerHTML += ` ${c} \u{1F449}`
             }
